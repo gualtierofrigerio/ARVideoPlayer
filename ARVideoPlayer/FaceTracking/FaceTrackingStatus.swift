cf20848fc893329da201ddce7b2d58d9c9b290f0
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-20 14:56:15 +0100

    added comments

--- ARVideoPlayer/FaceTracking/FaceTrackingStatus.swift
+++ ARVideoPlayer/FaceTracking/FaceTrackingStatus.swift
@@ -5,10 +5,11 @@
 //  Created by Gualtiero Frigerio on 20/02/21.
 //
 
+/// Describes the status of the Face tracking
 enum FaceTrackingStatus {
-    case faceDetected
-    case noFaceDetected
-    case idle
-    case ended
-    case error(Error)
+    case faceDetected // a face is detected
+    case noFaceDetected // no face detected
+    case idle // the tracker hasn't yet started
+    case ended // tracking ended
+    case error(Error) // reports an error
 }
