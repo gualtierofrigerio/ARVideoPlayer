1cd90bf6609a70f9b806dc5f1f07dfde88decb83
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-21 15:01:49 +0100

    added seek

--- ARVideoPlayer/VideoPlayer.swift
+++ ARVideoPlayer/VideoPlayer.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class VideoPlayer {
     
+    func goBackwards() {
+        seek(-30.0)
+    }
+    
+    func goForward() {
+        seek(30.0)
+    }
+    
     func openVideo(atURL url:URL) -> AVPlayerViewController {
         player = AVPlayer(url: url)
         let vc = AVPlayerViewController()
@@ -23,6 +31,20 @@ class VideoPlayer {
     
     func play() {
         player?.play()
+    }
+    
+    private func seek(_ value:Double) {
+        guard let player = player,
+              let duration = player.currentItem?.duration else { return }
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        let newTime = currentTime + value
+        if newTime < (CMTimeGetSeconds(duration) - value) {
+            let timeToGo: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64),
+                                              timescale: 1000)
+            player.seek(to: timeToGo,
+                        toleranceBefore: CMTime.zero,
+                        toleranceAfter: CMTime.zero)
+        }
     }
     
     private var player:AVPlayer?
