c4ca7e71bb44ea64039d34fda56b0af8520bd329
Gualtiero <gualtiero.frigerio@gmail.com>
2021-02-21 14:48:54 +0100

    added BlinkStatus

--- ARVideoPlayer/FaceTracking/FaceTrackingStatus.swift
+++ ARVideoPlayer/FaceTracking/FaceTrackingStatus.swift
@@ -5,8 +5,16 @@
 //  Created by Gualtiero Frigerio on 20/02/21.
 //
 
+enum BlinkStatus {
+    case leftEye
+    case rightEye
+    case bothEyes
+    case none
+}
+
 /// Describes the status of the Face tracking
 enum FaceTrackingStatus {
+    case blink(BlinkStatus)
     case faceDetected // a face is detected
     case noFaceDetected // no face detected
     case idle // the tracker hasn't yet started
